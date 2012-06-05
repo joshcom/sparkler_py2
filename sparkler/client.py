9aca3e1c1c3bd27c470e801a19b440d1ace7c1dc
Joshua Murray <joshua.murray@gmail.com>
2012-06-05 06:42:08 -0500

    Some early morning pylint refactoring

--- sparkler/client.py
+++ sparkler/client.py
@@ -1,33 +1,54 @@
+'''A Python client for Spark API: https://www.sparkplatform.com/docs
+'''
+
 from sparkler.transport import ApiRequest
-from sparkler.auth.factory import *
+from sparkler.auth.factory import AuthFactory
 
 class SparkClient:
+    '''The Spark API client.
+    
+    Public instance variables:
+    auth    -- The authentication/authorization interface.  See the corresponding
+               class or the README for more details.
+    request -- The ApiRequest object.  All useful methods are wrapped by 
+               the SparkClient object itself.
+    '''
     def __init__(self, client_key, client_secret, auth_mode="hybrid", 
             auth_endpoint_uri="https://sparkplatform.com/oauth2",
             api_endpoint_uri="https://sparkapi.com",
             auth_callback_uri=None,
-            data_access_version="v1",):
+            data_access_version="v1"):
         self.request = ApiRequest(api_endpoint_uri, data_access_version)
         self.auth = AuthFactory.create(auth_mode, client_key, client_secret,
                 auth_endpoint_uri, api_endpoint_uri, auth_callback_uri)
 
     def register_session(self, access_token, refresh_token):
+        '''Registers and existing session with the client.
+        
+        Arguments:
+        access_token -- The token allowing access to the API
+        refresh_token -- The token allowing a session to be refreshed.
+        '''
         pass
 
     ###
-    # TODO: These four methods need a better proxy to self.request.
-    #       Worst case scenario, this may as well just inherit ApiRequset
+    # These four methods need a better proxy to self.request.
+    # Worst case scenario, this may as well just inherit ApiRequset
     ###
-    def get(self,path,parameters=None):
+    def get(self, path, parameters=None):
+        '''A wrapper for self.request.get'''
         return self.request.get(path, parameters)
 
-    def post(self,path,body=None):
+    def post(self, path, body=None):
+        '''A wrapper for self.request.post'''
         return self.request.post(path, body)
 
-    def put(self,path,body=None):
+    def put(self, path, body=None):
+        '''A wrapper for self.request.put'''
         return self.request.put(path, body)
 
-    def delete(self,path,parameters=None):
+    def delete(self, path, parameters=None):
+        '''A wrapper for self.request.delete'''
         return self.request.delete(path, parameters)
 
 #    def __getattr__(self, attrib):
--- sparkler/errors.py
+++ sparkler/errors.py
@@ -1,6 +1,17 @@
+'''Exceptions raised by the Sparkler client.
+'''
+
 class ApplicationUnauthorizedException(Exception):
+    '''Raised when a request is made to the API when no authorization has been 
+    granted to the client.
+    '''
     pass
 class AuthExpiredException(Exception):
+    '''Raised when an authorization token has expired.  Signals the need
+    to refresh the session, if the particular auth method supports it.
+    '''
     pass
 class AuthFailureException(Exception):
+    '''Signals a general failure with an authorization attempt.
+    '''
     pass
